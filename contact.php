d397a75d8802feee8ad3b37699bdcdf10bb71a4c
ghost-mann <iamaob8@protonmail.com>
2025-12-11 13:36:55 +0300

    feat: added basic text,input fields and buttons in contact.php

--- contact.php
+++ contact.php
@@ -9,6 +9,24 @@
     <title>contact form</title>
 </head>
 <body>
+<div class="parent-container">
+    <div class="centered-div">
     <h1>Contact Form</h1>
+    <h3>First Name: </h3>
+    <label>
+        <input type="text">
+    </label>
+    <h3>Last Name: </h3>
+    <label>
+        <input type="text">
+    </label>
+    <h3>Email:</h3>
+    <label>
+        <input type=email>
+    </label>
+    <h3></h3>
+    </div>
+    <button type="button">SUBMIT</button>
+</div>
 </body>
 </html>
