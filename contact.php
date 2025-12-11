b8f4434b7adc7f174e6488ff003ca828c2403a5a
ghost-mann <iamaob8@protonmail.com>
2025-12-11 13:20:38 +0300

    initial commit

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact form</title>
+</head>
+<body>
+    <h1>Contact Form</h1>
+</body>
+</html>
