84a6fa94c87c584d45adfe3db409a90f191614c9
ghost-mann <iamaob8@protonmail.com>
2025-12-15 15:55:43 +0300

    feat: added basic validation to contact.php

--- contact.php
+++ contact.php
@@ -8,4 +8,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstName = trim($_POST['firstName'] ?? '');
     $lastName = trim($_POST['lastName'] ?? '');
     $email = trim($_POST['email'] ?? '');
+
+    // basic validation
+    if (empty($firstName) || empty($lastName) || empty($email)) {
+        echo "All fields must be filled";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+    }  else {
+        echo "Thanks for contacting us!";
+        echo "<p><strong>First Name:</strong>" . htmlspecialchars($firstName) . "</p>";
+        echo "<p><strong>Last Name:</strong>" . htmlspecialchars($lastName) . "</p>";
+        echo "<p><strong>Email:</strong>" . htmlspecialchars($email) . "</p>";
+    }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+</body>
+</html>
