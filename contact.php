44776536d1b539f68fa8b5049105d25c5ad8b3be
ghost-mann <iamaob8@protonmail.com>
2025-12-15 16:03:31 +0300

    feat: added basic html to contact.php

--- contact.php
+++ contact.php
@@ -26,8 +26,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form received</title>
 </head>
 <body>
+    <p><a href="index.php">← Back to form</a></p>
 </body>
 </html>
